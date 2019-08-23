12156aeca7cc69a1be17a1bc1bc2ea10d4f7cfa9
Scott Techau <scottbot95@gmail.com>
2019-08-23 13:47:38 -0700

    Download and include the BepInEx tooling in the distro release
    We probably need to include a patcher to load monomod still

--- scripts/release-distro.sh
+++ scripts/release-distro.sh
@@ -10,10 +10,19 @@ DISTRO_OUT="$OUT/Distro"
 # 0. cleanup
 rm -rf "$DISTRO_OUT"
 
+# 1. Download BepInEx
+extract-online-zip "https://github.com/BepInEx/BepInEx/releases/download/v5.0-RC1/BepInEx.5.0.RC1.v2017.x64.zip" "$DISTRO_OUT"
+
+# 2. Cleanup BepInEx files
+rm "$DISTRO_OUT/doorstop_config.ini" # Generated my Disunity.Managment
+mv $DISTRO_OUT/BepInEx/* $DISTRO_OUT
+rm -r "$DISTRO_OUT/BepInEx"
+
 # 1. Collect distro files
 core "Distro/core/"
 preloader-outs "Distro/patchers/"
 runtime-outs "Distro/plugins/"
+
 
 # 2. Zip up the distro
 ZIP_NAME=distro.zip
@@ -21,5 +30,5 @@ ZIP_NAME=distro.zip
 rm -f "$OUT/$ZIP_NAME"
 (cd "$DISTRO_OUT"; zip -r "$OUT/$ZIP_NAME" .)
 
-# 3. (Optional) cleanup uncompressed files
+# 3. cleanup files
 # rm -rf $DISTRO_OUT
--- scripts/release-funcs.sh
+++ scripts/release-funcs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUT="$SCRIPT_DIR/../Release"
-SOLUTION_ROOT="$SCRIPT_DIR/.."
+OUT=$(readlink -f "$SCRIPT_DIR/../Release")
+SOLUTION_ROOT=$(readlink -f "$SCRIPT_DIR/..")
 
 copy() {
     mkdir -p "$OUT/$2" && cp -r $1 "$_"
@@ -19,4 +19,24 @@ remove-unity-duplicates() {
     delete "$1/Microsoft.CSharp.dll"
     delete "$1/SemanticVersion.dll"
     delete "$1/System.Dynamic.Runtime.dll"
+}
+
+extract-online-zip() {
+    [ -z "$1" ] && return
+    local tmpfile=$(mktemp)
+
+    wget "$1" -O "$tmpfile"
+    if [ $? -ne 0 ]; then
+        echo "Failed to download bepinex"
+        exit 1
+    fi
+
+    if [ -z "$2" ]; then
+        local outdir=$(pwd)
+    else
+        local outdir="$2"
+    fi
+
+    mkdir -p "$outdir" && unzip -d "$outdir" "$tmpfile"
+    rm -f "$tmpfile"
 }
